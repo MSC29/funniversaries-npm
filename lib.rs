fb3cfd4ebb7d0bd7418797a7cd420d9184a22d76
Mael Sicard-Cras <mael.sicardcras@gmail.com>
2022-01-27 17:42:07 +0100

    sorting results by date

--- lib.rs
+++ lib.rs
@@ -10,9 +10,12 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 pub fn generate_anniversaries(date: &str) -> JsValue {
     web_sys::console::log_1(&date.clone().into());
-    let anniversaries = funniversaries_lib::find_anniversaries_future(date);
+    let mut anniversaries = funniversaries_lib::find_anniversaries_future(date);
     web_sys::console::log_1(&anniversaries[0].name.clone().into());
     web_sys::console::log_1(&anniversaries[0].date.to_rfc3339().clone().into());
+
+    // Sort people by age
+    anniversaries.sort_by(|a, b| b.date.cmp(&a.date));
 
     JsValue::from_serde(&anniversaries).unwrap()
 }
