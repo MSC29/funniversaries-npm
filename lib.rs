4a6bfc74914680522503b1b669a6d06d5e15642c
MSC <mael.sicardcras@gmail.com>
2022-01-28 11:28:07 +0100

    Feat/init (#1)
    Initial npm package

---
+++ lib.rs
@@ -0,0 +1,21 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn generate_anniversaries(date: &str) -> JsValue {
+    web_sys::console::log_1(&date.clone().into());
+    let mut anniversaries = funniversaries_lib::find_anniversaries_future(date);
+    web_sys::console::log_1(&anniversaries[0].name.clone().into());
+    web_sys::console::log_1(&anniversaries[0].date.to_rfc3339().clone().into());
+
+    // Sort people by age
+    anniversaries.sort_by(|a, b| b.date.cmp(&a.date));
+
+    JsValue::from_serde(&anniversaries).unwrap()
+}
