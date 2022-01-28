0009bdadbbde960bb0ad8d94f3e0b370342b3e68
MSC <mael.sicardcras@gmail.com>
2022-01-28 11:30:29 +0100

    Develop (#2)
    Initial npm package

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # funniversaries-npm
+
+Fun Anniversary npm library, WASM built from the rust lib (https://crates.io/crates/funniversaries)
+
+## Dev
+
+## build wasm
+
+```bash
+wasm-pack build
+```
---
+++ lib.rs
@@ -0,0 +1,18 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn generate_anniversaries(date: &str) -> JsValue {
+    let mut anniversaries = funniversaries_lib::find_anniversaries_future(date);
+
+    // Sort people by age
+    anniversaries.sort_by(|a, b| b.date.cmp(&a.date));
+
+    JsValue::from_serde(&anniversaries).unwrap()
+}
