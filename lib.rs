4ffa79887daf91ac678b1e9b2351fe1926982083
Mael Sicard-Cras <mael.sicardcras@gmail.com>
2022-04-13 00:06:50 +0200

    0.1.0

--- lib.rs
+++ lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use funniversaries::entities::anniversary::Anniversary;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
@@ -8,10 +9,28 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn generate_anniversaries(date: &str) -> JsValue {
-    let mut anniversaries = funniversaries::find_anniversaries_future(date);
+pub fn generate_future_anniversaries(date: &str) -> JsValue {
+    let anniversaries = funniversaries::find_anniversaries_future(date);
 
-    // Sort people by age
+    filter(anniversaries)
+}
+
+#[wasm_bindgen]
+pub fn generate_past_anniversaries(date: &str) -> JsValue {
+    let anniversaries = funniversaries::find_anniversaries_past(date);
+
+    filter(anniversaries)
+}
+
+#[wasm_bindgen]
+pub fn generate_all_anniversaries(date: &str) -> JsValue {
+    let anniversaries = funniversaries::find_anniversaries_all(date);
+
+    filter(anniversaries)
+}
+
+fn filter(mut anniversaries: Vec<Anniversary>) -> JsValue {
+    // Sort anniversaries by date
     anniversaries.sort_by(|a, b| b.date.cmp(&a.date));
 
     JsValue::from_serde(&anniversaries).unwrap()
